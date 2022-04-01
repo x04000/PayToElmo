940af7307d9bf7788e223dc846a1d8b74f332739
x04000 <78761999+x04000@users.noreply.github.com>
2022-04-01 21:00:58 +0200

    Update PayToElmo.js

--- PayToElmo.js
+++ PayToElmo.js
@@ -6,6 +6,7 @@ if (codigoverificacion == "ABC123") {
     alert("Muchas gracias por esos ricos 99999999€ bro :)")
 }
 else {
+    document.write('<title>Elmo comunista D:</title>')
     document.write('<h1 style="color:RED">POR NO PAGAR, AHORA <u>ELMO RUSO COMUNISTA 100% REAL NO FAKE 1 LINK MEGA NO VIRUSES</u> TE HA HACKIADO</h1>')
     document.write('<img src="https://pbs.twimg.com/profile_images/1496525052073889797/Xpcij1wa_400x400.jpg" width="1280" height="720">')
     document.write('<iframe width="560" height="315" src="https://www.youtube.com/embed/2RlTqFtdlH8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
